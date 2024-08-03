c563457e9f73cca0e53b6de18e6fadff3b990b5d
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-08-03 08:04:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -15,6 +15,7 @@ BATCH_FILES = {}
 
 @Client.on_message(filters.command("start") & filters.incoming)
 async def start(client, message):
+    await message.react(emoji="🔥", big=True)
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[           
             InlineKeyboardButton('📢 Uᴩᴅᴀᴛᴇꜱ 📢', url=f'https://t.me/{SUPPORT_CHAT}')
