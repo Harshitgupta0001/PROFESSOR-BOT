85d5d5255926ff72989d9deaa8a019a8f806ef38
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-09-09 13:12:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,11 +32,11 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "?retryWrites=ster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "0")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://Src:Bot@cluster0.doyhu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
 FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'professor_files')
 
 # Filters Configuration 
 MAX_RIST_BTNS = int(environ.get('MAX_RIST_BTNS', "10"))
